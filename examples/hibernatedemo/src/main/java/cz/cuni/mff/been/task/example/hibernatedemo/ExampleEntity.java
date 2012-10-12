c518996fac9b404b9ff7eba743a493eb4d0bbc48
Martin Sixta <martin.sixta@gmail.com>
2012-10-12 21:07:49 +0200

    Examples re-added.
    fixes #49

---
+++ examples/hibernatedemo/src/main/java/cz/cuni/mff/been/task/example/hibernatedemo/ExampleEntity.java
@@ -0,0 +1,60 @@
+/*
+ *  BEEN: Benchmarking Environment
+ *  ==============================
+ *
+ *  File author: Jan Tattermusch
+ *
+ *  GNU Lesser General Public License Version 2.1
+ *  ---------------------------------------------
+ *  Copyright (C) 2004-2006 Distributed Systems Research Group,
+ *  Faculty of Mathematics and Physics, Charles University in Prague
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License version 2.1, as published by the Free Software Foundation.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA  02111-1307  USA
+ */
+
+package cz.cuni.mff.been.task.example.hibernatedemo;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+
+/**
+ * Example annotated class.
+ * @author Jan Tattermusch
+ *
+ */
+@Entity
+public class ExampleEntity {
+
+    private Long id;
+    private String name;
+    
+    @Id
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
