59aa23403084b063a3d66c43fc8beaeede335c2d
donarus <tadeas@palusga.cz>
2013-04-13 20:35:10 +0200

    InprocMessageQueueTest refactored

--- core-utils/src/test/java/cz/cuni/mff/d3s/been/mq/InprocMessageQueueTest.java
+++ core-utils/src/test/java/cz/cuni/mff/d3s/been/mq/InprocMessageQueueTest.java
@@ -6,7 +6,8 @@ import static cz.cuni.mff.d3s.been.mq.TestParams.INPROC_QUEUE_NAME;
  * @author Martin Sixta
  */
 public class InprocMessageQueueTest extends BasicQueueTests {
+	@Override
 	protected IMessageQueue<String> getQueue() {
-		return Messaging.createInprocQueue(INPROC_QUEUE_NAME);
+		return new InprocMessageQueue<String>(INPROC_QUEUE_NAME);
 	}
 }
