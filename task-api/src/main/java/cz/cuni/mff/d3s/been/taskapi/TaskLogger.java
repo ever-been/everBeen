b5f3f4cd438d44e2f99c9ef9b6cbab2a7a17df7e
Martin Sixta <martin.sixta@gmail.com>
2013-04-21 19:31:17 +0200

    Task Logging    * TaskLoggers are now customized by name    * log messages now include context ID, thread name, name of the logger, and time
    relates to #72

--- task-api/src/main/java/cz/cuni/mff/d3s/been/taskapi/TaskLogger.java
+++ task-api/src/main/java/cz/cuni/mff/d3s/been/taskapi/TaskLogger.java
@@ -1,6 +1,7 @@
 package cz.cuni.mff.d3s.been.taskapi;
 
 import static cz.cuni.mff.d3s.been.core.TaskMessageType.LOG_MESSAGE;
+import static cz.cuni.mff.d3s.been.core.TaskPropertyNames.TASK_CONTEXT_ID;
 import static cz.cuni.mff.d3s.been.core.TaskPropertyNames.TASK_ID;
 import cz.cuni.mff.d3s.been.core.LogMessage;
 import cz.cuni.mff.d3s.been.core.utils.JSONUtils;
@@ -11,10 +12,18 @@ import cz.cuni.mff.d3s.been.core.utils.JSONUtils;
 public class TaskLogger extends TaskLoggerBase {
 
 	private static String senderId;
+	private static String contextId;
+
+	private String name;
 
 	static {
-		// TODO sixtam Handle property not set
 		senderId = System.getenv(TASK_ID);
+		contextId = System.getenv(TASK_CONTEXT_ID);
+	}
+
+	public TaskLogger(String name) {
+
+		this.name = name;
 	}
 
 	/**
@@ -33,7 +42,7 @@ public class TaskLogger extends TaskLoggerBase {
 	void log(int level, String message, Throwable t) {
 		try {
 
-			LogMessage logMsg = new LogMessage(level, message, t, senderId);
+			LogMessage logMsg = new LogMessage(name, level, message, t, senderId, contextId);
 			String serializedMsg = JSONUtils.serialize(logMsg);
 			String msg = String.format("%s#%s", LOG_MESSAGE.toString(), serializedMsg);
 
--- task-api/src/main/java/cz/cuni/mff/d3s/been/taskapi/TaskLoggerFactory.java
+++ task-api/src/main/java/cz/cuni/mff/d3s/been/taskapi/TaskLoggerFactory.java
@@ -4,18 +4,13 @@ import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 
 /**
- * Created with IntelliJ IDEA.
- * User: Kuba
- * Date: 11.03.13
- * Time: 11:45
- * To change this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. User: Kuba Date: 11.03.13 Time: 11:45 To change
+ * this template use File | Settings | File Templates.
  */
 public class TaskLoggerFactory implements ILoggerFactory {
 
-	private static TaskLogger logger = new TaskLogger();
-
 	@Override
 	public Logger getLogger(String name) {
-		return logger;
+		return new TaskLogger(name);
 	}
 }
