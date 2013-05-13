cbf7b724586feb05ebfe4576dd2db77f68eb746a
donarus <tadeas@palusga.cz>
2013-05-13 13:56:21 +0200

    beanproperty edit blocks, beenBeanEditForm, beenPropertyEditor

---
+++ web-interface/src/main/java/cz/cuni/mff/d3s/been/web/components/BeenBeanEditForm.java
@@ -0,0 +1,6 @@
+package cz.cuni.mff.d3s.been.web.components;
+
+import org.apache.tapestry5.corelib.components.BeanEditForm;
+
+public class BeenBeanEditForm extends BeanEditForm {
+}
---
+++ web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/PropertyEditBlocks.java
@@ -0,0 +1,26 @@
+package cz.cuni.mff.d3s.been.web.pages;
+
+
+/**
+ * <b style="color:red">DO NOT RENAME AND DO NOT MOVE THIS CLASS</b>
+ *
+ *
+ * <p>This class is only wrapper around original PropertyEditBlocks page.
+ * (It rewrites the original PropertyEditBlocks in terms of tapestry components.)
+ * </p>
+ * <p>How it works? When T5 is building any BeanEditForm component, it searches for
+ * PropertyEditBlocks page, where are defined templates for all property edit
+ * blocks (for example for strings, booleans, numbers, enums etc..). If we redefine
+ * (rewrite) this page, tapestry will use our provided templates instead of original
+ * templates.<p/>
+ * <strong>So what??</strong>
+ * <p style="color:red">SO DO NOT RENAME AND DO NOT MOVE THIS CLASS !!!!!!!!!!!!!!!</p>
+ * <p style="color:red">SO DO NOT RENAME AND DO NOT MOVE THIS CLASS !!!!!!!!!!!!!!!</p>
+ * <p style="color:red">SO DO NOT RENAME AND DO NOT MOVE THIS CLASS !!!!!!!!!!!!!!!</p>
+ * <p>
+ * Tml for PropertyEditBlocks component is searched in the same package, as
+ * tThis class is used to redefine default PropertyEditBlocksDO NOT RENAME THIS CLASS
+ * </p>
+ */
+public class PropertyEditBlocks extends org.apache.tapestry5.corelib.pages.PropertyEditBlocks {
+}
