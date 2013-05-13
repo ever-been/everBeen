1fca94cf77b89a1aaad329f42f5bf6d87c0c159c
donarus <tadeas@palusga.cz>
2013-05-13 20:28:43 +0200

    SubmitTaskDescriptor page

--- web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/task/SubmitTaskDescriptor.java
+++ web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/task/SubmitTaskDescriptor.java
@@ -1,7 +1,9 @@
 package cz.cuni.mff.d3s.been.web.pages.task;
 
 import cz.cuni.mff.d3s.been.bpk.BpkIdentifier;
+import cz.cuni.mff.d3s.been.core.task.ObjectFactory;
 import cz.cuni.mff.d3s.been.core.task.TaskDescriptor;
+import cz.cuni.mff.d3s.been.core.task.TaskProperty;
 import cz.cuni.mff.d3s.been.web.components.Layout;
 import cz.cuni.mff.d3s.been.web.pages.Overview;
 import cz.cuni.mff.d3s.been.web.pages.Page;
@@ -10,40 +12,100 @@ import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Form;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 /**
+ * This component is used to edit task descriptor properties and submit
+ * task defined by this task descriptor.
+ *
  * @author Kuba Brecka
+ * @author Tadeas Palusga
  */
 @Page.Navigation(section = Layout.Section.TASK_SUBMIT)
+@SuppressWarnings("unused")
 public class SubmitTaskDescriptor extends Page {
 
-	@Property
-	BpkIdentifier bpkIdentifier;
-
+	/**
+	 * Task descriptor loaded in onActivate() method
+	 */
 	@Property
 	@Persist
 	TaskDescriptor taskDescriptor;
 
+	/**
+	 * Variable is used as property in AjaxFormLoop via argument wrappers used in tmlTemplate
+	 */
+	@Property
+	@SuppressWarnings("unused")
+	private String arg;
+
+	@Property
+	@SuppressWarnings("unused")
+	private int propertyLoopIndex;
+
+	/**
+	 * Setup method. Loads task descriptor (corresponding to given parameters)
+	 * using been api and sets this descriptor as editable property for this component.
+	 *
+	 * @param groupId        group id of {@link BpkIdentifier} to which the underlying {@link TaskDescriptor} belows
+	 * @param bpkId          bpk id of {@link BpkIdentifier} to which the underlying {@link TaskDescriptor} belows
+	 * @param version        version id of {@link BpkIdentifier} to which the underlying {@link TaskDescriptor} belows
+	 * @param descriptorName name of concrete {@link TaskDescriptor} for {@link BpkIdentifier} identified by previous parameters
+	 */
+	@SuppressWarnings("unused")
 	void onActivate(String groupId, String bpkId, String version, String descriptorName) {
-		bpkIdentifier = new BpkIdentifier();
+		// load correct task descriptor
+		BpkIdentifier bpkIdentifier = new BpkIdentifier();
 		bpkIdentifier.setGroupId(groupId);
 		bpkIdentifier.setBpkId(bpkId);
 		bpkIdentifier.setVersion(version);
-
 		this.taskDescriptor = this.api.getApi().getTaskDescriptor(bpkIdentifier, descriptorName);
+
+		// initialize fields on loaded task descriptor if not initialized
+		if (!taskDescriptor.isSetArguments()) {
+			taskDescriptor.setArguments(new ObjectFactory().createArguments());
+		}
+
+		if (!taskDescriptor.isSetProperties()) {
+			taskDescriptor.setProperties(new ObjectFactory().createTaskProperties());
+		}
 	}
 
-	Object onPassivate() {
-		return new Object[]{bpkIdentifier.getGroupId(), bpkIdentifier.getBpkId(), bpkIdentifier
-				.getVersion(), taskDescriptor.getName()};
-	}
 
+	/**
+	 * Injected form component for task submitting.
+	 */
 	@Component
-	private Form form;
+	@SuppressWarnings("unused")
+	private Form submitTaskForm;
 
-	Object onSuccess() {
+	/**
+	 * This handler is called when users click on form SUBMIT button.
+	 * Submits task to BEEN cluster using {@link cz.cuni.mff.d3s.been.api.BeenApi}
+	 *
+	 * @return redirect to {@link Overview} page
+	 */
+	@SuppressWarnings("unused")
+	Object onSuccessFromSubmitTaskForm() {
 		this.api.getApi().submitTask(taskDescriptor);
 		return Overview.class;
 	}
 
+	// -----------------------------
+	// HANDLING ARGUMENTS
+	// -----------------------------
+
+	/**
+	 * This handler is invoked when user click on the ADD (+) button in arguments section.
+	 * Returns new default argument (empty string)
+	 *
+	 * @return empty argument
+	 */
+	@SuppressWarnings("unused")
+	String onAddRowFromArgumentList() {
+		return "";
+	}
 
 }
