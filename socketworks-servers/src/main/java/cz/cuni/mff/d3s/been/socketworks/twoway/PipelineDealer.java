181eb5a9408125801916b5d57edea736f38c3829
Martin Sixta <martin.sixta@gmail.com>
2013-07-20 15:30:15 +0200

    Workaround for JeroMQ big message bug   * this is temporary solution
     JSONResultFacade refactored and made more thread safe

--- socketworks-servers/src/main/java/cz/cuni/mff/d3s/been/socketworks/twoway/PipelineDealer.java
+++ socketworks-servers/src/main/java/cz/cuni/mff/d3s/been/socketworks/twoway/PipelineDealer.java
@@ -2,13 +2,13 @@ package cz.cuni.mff.d3s.been.socketworks.twoway;
 
 import java.util.Iterator;
 
-import cz.cuni.mff.d3s.been.socketworks.Socketworks;
 import org.jeromq.ZMQ;
 import org.jeromq.ZMQ.Socket;
 
 import cz.cuni.mff.d3s.been.mq.Context;
 import cz.cuni.mff.d3s.been.mq.MessagingException;
 import cz.cuni.mff.d3s.been.mq.ZMQContext;
+import cz.cuni.mff.d3s.been.socketworks.Socketworks;
 
 /**
  * A (maybe unnecessary) implementation of a 0MQ dealer. Its job is to convey
@@ -40,11 +40,25 @@ class PipelineDealer extends PollPartaker {
 		final ZMQContext zctx = Context.getReference();
 		final Socket sendReplySocket = zctx.socket(ZMQ.DEALER);
 		final Iterator<byte[]> fi = frames.iterator();
-		sendReplySocket.connect(Socketworks.Protocol.TCP.connection(hostname,port));
+
+		sendReplySocket.connect(Socketworks.Protocol.TCP.connection(hostname, port));
+
+		int lastFrameSize = 0;
 		while (fi.hasNext()) {
 			final byte[] frame = fi.next();
 			sendReplySocket.send(frame, fi.hasNext() ? ZMQ.SNDMORE : NOFLAGS);
+			lastFrameSize = frame.length;
 		}
+
+		// JeroMQ big message bug workaround (big messages not transmitted completely)
+		if (lastFrameSize > 100_000) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				//quell
+			}
+		}
+
 		sendReplySocket.close();
 	}
 
