69285e0ed8aef0e3e0e06d0f0122eb617fce24ce
Kuba Brecka <kuba.brecka@gmail.com>
2013-07-21 14:18:41 +0200

    Web Interface * runtime list page

--- web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/Page.java
+++ web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/Page.java
@@ -191,4 +191,12 @@ public abstract class Page {
 		return Integer.toString(logLevel);
 	}
 
+	// stolen from http://stackoverflow.com/questions/3263892/format-file-size-as-mb-gb-etc
+	public String bytesReadable(long size) {
+		if(size <= 0) return "0";
+		final String[] units = new String[] { "B", "KB", "MB", "GB", "TB" };
+		int digitGroups = (int) (Math.log10(size)/Math.log10(1024));
+		return new DecimalFormat("#,##0.#").format(size/Math.pow(1024, digitGroups)) + " " + units[digitGroups];
+	}
+
 }
