7b8e5f4ecf6a93a531b2198660af0530b093f88c
darklight <darklight@darklight.cz>
2013-07-27 17:01:40 +0200

    Extract cluster configuration.
    relates to #114

---
+++ core-cluster/src/main/java/cz/cuni/mff/d3s/been/cluster/ClusterClientConfiguration.java
@@ -0,0 +1,22 @@
+package cz.cuni.mff.d3s.been.cluster;
+
+import cz.cuni.mff.d3s.been.BeenServiceConfiguration;
+
+/**
+ * BEEN cluster client configuration
+ *
+ * @author darklight
+ */
+public class ClusterClientConfiguration extends BeenServiceConfiguration {
+	private ClusterClientConfiguration() {}
+
+	/** Property for the timeout of the native client connection (in seconds). Hazelcast tends to disconnect/reconnect clients too often with default settings. */
+	public static final String TIMEOUT = "been.cluster.client.timeout";
+	/** By default, a native Hazelcast client's session will remain active for {@value #DEFAULT_TIMEOUT} seconds */
+	public static final Integer DEFAULT_TIMEOUT = 120;
+
+	/** Property with a ';' separated list of Hazelcast members which will be contacted in attempt to join the BEEN Hazelcast cluster. */
+	public static final String MEMBERS = "been.cluster.client.members";
+	/** By default, BEEN will attempt to join these members: {@value #DEFAULT_MEMBERS} */
+	public static final String DEFAULT_MEMBERS = "localhost:5701";
+}
