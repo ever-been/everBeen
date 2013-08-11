d6c6dc6b8f3b5495c99c3b51781f624e68391106
Kuba Brecka <kuba.brecka@gmail.com>
2013-08-11 12:58:48 +0200

    Implement maximum resubmit limit.

---
+++ task-manager/src/main/java/cz/cuni/mff/d3s/been/task/action/ResubmitActionConfiguration.java
@@ -0,0 +1,15 @@
+package cz.cuni.mff.d3s.been.task.action;
+
+import cz.cuni.mff.d3s.been.BeenServiceConfiguration;
+
+/**
+ * @author Kuba Brecka
+ */
+public class ResubmitActionConfiguration extends BeenServiceConfiguration {
+	private ResubmitActionConfiguration() {}
+
+	/** The maximum number of benchmark generator resubmits before aborting the whole benchmark. */
+	public static final String MAXIMUM_ALLOWED_RESUBMITS = "been.cluster.resubmit.maximum-allowed";
+	/** The default maximum number of resubmits is '{@value #DEFAULT_MAXIMUM_ALLOWED_RESUBMITS}'. */
+	public static final Integer DEFAULT_MAXIMUM_ALLOWED_RESUBMITS = 10;
+}
