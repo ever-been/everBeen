676daa7cf09718e08a3e4bf5bbec9c04df1a565a
Martin Sixta <martin.sixta@gmail.com>
2013-08-18 13:16:26 +0200

    Fixes Janitor crash    * wrong collection was used to get value from

--- repository/src/main/java/cz/cuni/mff/d3s/been/repository/janitor/TrashProcessor.java
+++ repository/src/main/java/cz/cuni/mff/d3s/been/repository/janitor/TrashProcessor.java
@@ -1,15 +1,15 @@
 package cz.cuni.mff.d3s.been.repository.janitor;
 
-import cz.cuni.mff.d3s.been.persistence.task.PersistentContextState;
-import cz.cuni.mff.d3s.been.persistence.task.PersistentTaskState;
-
 import java.util.Collection;
 import java.util.Map;
 import java.util.TreeMap;
 
+import cz.cuni.mff.d3s.been.persistence.task.PersistentContextState;
+import cz.cuni.mff.d3s.been.persistence.task.PersistentTaskState;
+
 /**
  * Persistence trash cleanup planner
- *
+ * 
  * @author darklight
  */
 class TrashProcessor {
@@ -18,11 +18,12 @@ class TrashProcessor {
 
 	/**
 	 * Add newly retrieved task outcomes from the persistence layer
-	 *
-	 * @param states States to add
+	 * 
+	 * @param states
+	 *          States to add
 	 */
 	public void addTaskStates(Collection<PersistentTaskState> states) {
-		for(PersistentTaskState state: states) {
+		for (PersistentTaskState state : states) {
 			TotalOutcome<PersistentTaskState> outcome = oldTasks.get(state.getTaskId());
 			if (outcome == null) {
 				outcome = new TotalTaskOutcome();
@@ -34,11 +35,12 @@ class TrashProcessor {
 
 	/**
 	 * Add newly retrieved context outcomes from the persistence layer
-	 *
-	 * @param states States to add
+	 * 
+	 * @param states
+	 *          States to add
 	 */
 	public void addContextStates(Collection<PersistentContextState> states) {
-		for(PersistentContextState state: states) {
+		for (PersistentContextState state : states) {
 			TotalOutcome<PersistentContextState> outcome = oldContexts.get(state.getContextId());
 			if (outcome == null) {
 				outcome = new TotalContextOutcome();
@@ -50,19 +52,19 @@ class TrashProcessor {
 
 	/**
 	 * Get a task outcome for processing
-	 *
+	 * 
 	 * @return A task outcome to be processed
 	 */
 	public synchronized TotalOutcome<PersistentTaskState> getNextTask() {
 		if (oldTasks.isEmpty()) {
 			return null;
 		}
-		return oldTasks.remove(oldContexts.keySet().iterator().next());
+		return oldTasks.remove(oldTasks.keySet().iterator().next());
 	}
 
 	/**
 	 * Get a context outcome for processing
-	 *
+	 * 
 	 * @return A context outcome to be processed
 	 */
 	public synchronized TotalOutcome<PersistentContextState> getNextContext() {
