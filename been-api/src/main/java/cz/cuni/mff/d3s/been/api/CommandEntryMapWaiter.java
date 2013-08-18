94692249324e85bb13654f9c355d803f13f278f2
Kuba Brecka <kuba.brecka@gmail.com>
2013-08-18 13:47:27 +0200

    Javadoc - been-api module is now 100% documented

--- been-api/src/main/java/cz/cuni/mff/d3s/been/api/CommandEntryMapWaiter.java
+++ been-api/src/main/java/cz/cuni/mff/d3s/been/api/CommandEntryMapWaiter.java
@@ -1,67 +1,76 @@
 package cz.cuni.mff.d3s.been.api;
 
+import java.util.Queue;
+
 import com.hazelcast.core.EntryEvent;
 import com.hazelcast.core.EntryListener;
 import cz.cuni.mff.d3s.been.core.protocol.command.CommandEntry;
 
-import java.util.Queue;
-
-
 /**
- * Hazelcast map entry listener for {@link CommandEntry} entities, which listens for
- * UPDATE / REMOVE / EVICT operations. ADD operation is quietly ignored.
- * <br/><br/>
+ * Hazelcast map entry listener for {@link CommandEntry} entities, which listens
+ * for UPDATE / REMOVE / EVICT operations. ADD operation is quietly ignored. <br/>
+ * <br/>
  * Each updated/removed/evicted entry is added to queue given in constructor.
- *
+ * 
  * @author donarus
  */
 final class CommandEntryMapWaiter implements EntryListener<Long, CommandEntry> {
 
-    private Queue queue;
+	/** the queue into which the event should be added */
+	private Queue queue;
 
+	/**
+	 * Default constructor, creates an instance with the specified queue.
+	 * 
+	 * @param queue
+	 *          the queue into which the event should be added
+	 */
+	public CommandEntryMapWaiter(Queue<CommandEntry> queue) {
+		this.queue = queue;
+	}
 
-    public CommandEntryMapWaiter(Queue<CommandEntry> queue) {
-        this.queue = queue;
-    }
+	/**
+	 * IGNORED - just must be here because of parent interface
+	 * 
+	 * @param event
+	 *          the event that occurred
+	 */
+	@Override
+	public final void entryAdded(EntryEvent<Long, CommandEntry> event) {
+		// ignore
+	}
 
-    /**
-     * IGNORED - just must be here because of parent interface
-     *
-     * @param event
-     */
-    @Override
-    public final void entryAdded(EntryEvent<Long, CommandEntry> event) {
-        // ignore
-    }
+	/**
+	 * Adds value from given event to queue
+	 * 
+	 * @param event
+	 *          the event that occurred
+	 */
+	@Override
+	public final void entryRemoved(EntryEvent<Long, CommandEntry> event) {
+		queue.add(event.getValue());
+	}
 
-    /**
-     * Adds value from given event to queue
-     *
-     * @param event
-     */
-    @Override
-    public final void entryRemoved(EntryEvent<Long, CommandEntry> event) {
-        queue.add(event.getValue());
-    }
+	/**
+	 * Adds value from given event to queue
+	 * 
+	 * @param event
+	 *          the event that occurred
+	 */
+	@Override
+	public final void entryUpdated(EntryEvent<Long, CommandEntry> event) {
+		queue.add(event.getValue());
+	}
 
-    /**
-     * Adds value from given event to queue
-     *
-     * @param event
-     */
-    @Override
-    public final void entryUpdated(EntryEvent<Long, CommandEntry> event) {
-        queue.add(event.getValue());
-    }
-
-    /**
-     * Adds value from given event to queue
-     *
-     * @param event
-     */
-    @Override
-    public void entryEvicted(EntryEvent<Long, CommandEntry> event) {
-        queue.add(event.getValue());
-    }
+	/**
+	 * Adds value from given event to queue
+	 * 
+	 * @param event
+	 *          the event that occurred
+	 */
+	@Override
+	public void entryEvicted(EntryEvent<Long, CommandEntry> event) {
+		queue.add(event.getValue());
+	}
 
 }
--- been-api/src/main/java/cz/cuni/mff/d3s/been/api/CommandTimeoutException.java
+++ been-api/src/main/java/cz/cuni/mff/d3s/been/api/CommandTimeoutException.java
@@ -1,22 +1,42 @@
 package cz.cuni.mff.d3s.been.api;
 
 /**
- * This exception should be thrown no command execution timeout.
- *
+ * This exception should be thrown when a command execution times out.
+ * 
  * @author donarus
  */
 public class CommandTimeoutException extends BeenApiException {
 
-    public CommandTimeoutException(String message) {
-        super(message);
-    }
+	/**
+	 * Constructs a new exception with the specified detail message.
+	 * 
+	 * @param message
+	 *          the detail message
+	 */
+	public CommandTimeoutException(String message) {
+		super(message);
+	}
 
-    public CommandTimeoutException(String message, Throwable cause) {
-        super(message, cause);
-    }
+	/**
+	 * Constructs a new exception with the specified detail message and cause.
+	 * 
+	 * @param message
+	 *          the detail message
+	 * @param cause
+	 *          cause of the exception
+	 */
+	public CommandTimeoutException(String message, Throwable cause) {
+		super(message, cause);
+	}
 
-    public CommandTimeoutException(Throwable cause) {
-        super(cause);
-    }
+	/**
+	 * Constructs a new exception with the specified {@link Throwable} as a cause.
+	 * 
+	 * @param cause
+	 *          cause of the exception
+	 */
+	public CommandTimeoutException(Throwable cause) {
+		super(cause);
+	}
 
 }
