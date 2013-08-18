d746ba7602581ef1d25bc8da24128cee7a834d12
Martin Sixta <martin.sixta@gmail.com>
2013-08-18 16:02:07 +0200

    Fixes persistent tasks/contexts not setting created field    * it caused janitor to clean-up everything

--- persistence/src/main/java/cz/cuni/mff/d3s/been/persistence/task/PersistentTaskState.java
+++ persistence/src/main/java/cz/cuni/mff/d3s/been/persistence/task/PersistentTaskState.java
@@ -5,7 +5,7 @@ import cz.cuni.mff.d3s.been.core.task.TaskState;
 
 /**
  * A persistent carrie object for {@link TaskState}
- *
+ * 
  * @author darklight
  */
 public class PersistentTaskState extends TaskEntity {
@@ -13,37 +13,43 @@ public class PersistentTaskState extends TaskEntity {
 	private long timeStarted;
 	private long timeFinished;
 	private String runtimeId;
-    private TaskState taskState;
+	private TaskState taskState;
 
-    /**
-     * Get the state with which this task has finished
-     *
-     * @return The task state
-     */
-    public TaskState getTaskState() {
-        return taskState;
-    }
+	public PersistentTaskState() {
+		created = System.currentTimeMillis();
+	}
 
-    /**
-     * Set the state with which given task has finished
-     *
-     * @param taskState State to set
-     */
-    public void setTaskState(TaskState taskState) {
-        this.taskState = taskState;
-    }
+	/**
+	 * Get the state with which this task has finished
+	 * 
+	 * @return The task state
+	 */
+	public TaskState getTaskState() {
+		return taskState;
+	}
 
-    /**
-     * Fluently set the state with which given task has finished
-     *
-     * @param taskState State to set
-     *
-     * @return This {@link PersistentTaskState}, with changed task state
-     */
-    public PersistentTaskState withTaskState(TaskState taskState) {
-        setTaskState(taskState);
-        return this;
-    }
+	/**
+	 * Set the state with which given task has finished
+	 * 
+	 * @param taskState
+	 *          State to set
+	 */
+	public void setTaskState(TaskState taskState) {
+		this.taskState = taskState;
+	}
+
+	/**
+	 * Fluently set the state with which given task has finished
+	 * 
+	 * @param taskState
+	 *          State to set
+	 * 
+	 * @return This {@link PersistentTaskState}, with changed task state
+	 */
+	public PersistentTaskState withTaskState(TaskState taskState) {
+		setTaskState(taskState);
+		return this;
+	}
 
 	public long getTimeStarted() {
 		return timeStarted;
