37a806a04a4fa53fbd06a88ddf4238b3f4d081d6
Martin Sixta <martin.sixta@gmail.com>
2013-08-19 09:31:26 +0200

    Workaround for #188    * do not log Hazelcast messages with BeenAppender
    relates to #188

--- logback-appender/src/main/java/cz/cuni/mff/d3s/been/logging/BeenAppender.java
+++ logback-appender/src/main/java/cz/cuni/mff/d3s/been/logging/BeenAppender.java
@@ -1,13 +1,13 @@
 package cz.cuni.mff.d3s.been.logging;
 
+import java.util.ServiceLoader;
+
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 
-import java.util.ServiceLoader;
-
 /**
  * Logback appender to BEEN shared memory
- *
+ * 
  * @author darklight
  */
 public class BeenAppender extends AppenderBase<ILoggingEvent> {
@@ -23,15 +23,26 @@ public class BeenAppender extends AppenderBase<ILoggingEvent> {
 	@Override
 	protected void append(ILoggingEvent eventObject) {
 		if (logHandler == null) {
-			System.err.println(String.format("Could not log following message, because no '%s' instance was found:\n%s", ServiceLogHandler.class.getSimpleName(), eventObject.getMessage()));
+			System.err.println(String.format(
+					"Could not log following message, because no '%s' instance was found:\n%s",
+					ServiceLogHandler.class.getSimpleName(),
+					eventObject.getMessage()));
 		}
+
+		final String loggerName = eventObject.getLoggerName();
+
+		// workaround for issue #188
+		if (loggerName != null && loggerName.startsWith("com.hazelcast")) {
+			return;
+		}
+
 		final LogMessage message = new LogMessage();
 		message.setLevel(levelConverter.getBeenLogLevel(eventObject.getLevel()));
 		message.setMessage(eventObject.getFormattedMessage());
 		message.setName(eventObject.getLoggerName());
 		message.setThreadName(eventObject.getThreadName());
 		final StringBuilder traceBuilder = new StringBuilder();
-		for (StackTraceElement traceElm: eventObject.getCallerData()) {
+		for (StackTraceElement traceElm : eventObject.getCallerData()) {
 			traceBuilder.append(traceElm.toString());
 			traceBuilder.append(Character.LINE_SEPARATOR);
 		}
@@ -39,7 +50,9 @@ public class BeenAppender extends AppenderBase<ILoggingEvent> {
 		try {
 			logHandler.log(message);
 		} catch (Exception e) {
-			System.err.println(String.format("Could not log following message because of an exception:\n%s", message.toString()));
+			System.err.println(String.format(
+					"Could not log following message because of an exception:\n%s",
+					message.toString()));
 			e.printStackTrace();
 		}
 	}
