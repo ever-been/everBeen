cb400769ec4c1a2440b5d2ef3247729ab0233b3b
Martin Sixta <martin.sixta@gmail.com>
2013-08-29 13:52:15 +0200

    Removes so-called "python-task-test" module

--- python-task-test/test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "error" | python $logger -e
-echo "warn"  | python $logger -w
-echo "info"  | python $logger -i
-echo "debug" | python $logger -d
-echo "trace" | python $logger -t
