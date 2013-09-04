20430dd51a335aa89a13b6a8b809334fcf0a6edd
donarus <tadeas@palusga.cz>
2013-09-04 16:06:28 +0200

    BUGFIX - fixed invalid url to download logs (this error occured in external servlet containers with different context url than '/' e.g. in tomcat)

--- web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/cluster/Logs.java
+++ web-interface/src/main/java/cz/cuni/mff/d3s/been/web/pages/cluster/Logs.java
@@ -1,19 +1,76 @@
 package cz.cuni.mff.d3s.been.web.pages.cluster;
 
-import cz.cuni.mff.d3s.been.api.BeenApiException;
-import cz.cuni.mff.d3s.been.api.ClusterConnectionUnavailableException;
-import cz.cuni.mff.d3s.been.web.components.Layout;
-import cz.cuni.mff.d3s.been.web.pages.Page;
-import org.apache.tapestry5.annotations.Property;
-
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
-import java.util.List;
+
+import org.apache.tapestry5.ComponentResources;
+import org.apache.tapestry5.Link;
+import org.apache.tapestry5.StreamResponse;
+import org.apache.tapestry5.ioc.annotations.Inject;
+import org.apache.tapestry5.services.Response;
+
+import cz.cuni.mff.d3s.been.logging.ServiceLogMessage;
+import cz.cuni.mff.d3s.been.web.components.Layout;
+import cz.cuni.mff.d3s.been.web.pages.Page;
 
 /**
  * @author Kuba Brecka
  */
 @Page.Navigation(section = Layout.Section.CLUSTER_LOGS)
 public class Logs extends Page {
+
+	public static final String DATE_FORMAT = "yyyy-MM-dd";
+
+	@Inject
+	private ComponentResources resources;
+
+	public Link getDownloadEventBaseLink() {
+		return resources.createEventLink("downloadLogs");
+	}
+
+	public Object onDownloadLogs(String dateString) throws Exception {
+		Date date;
+		try {
+			date = new SimpleDateFormat(DATE_FORMAT).parse(dateString);
+		} catch (ParseException e) {
+			throw new Exception(String.format(
+					"Format of the given date is invalid. Expected format '%s' given value '%s'",
+					DATE_FORMAT,
+					dateString));
+		}
+
+		Collection<ServiceLogMessage> serviceLogs = this.api.getApi().getServiceLogsByDate(date);
+		StringBuilder sb = new StringBuilder();
+		for (ServiceLogMessage serviceLog : serviceLogs) {
+			sb.append(serviceLog.toDownloadableString());
+			sb.append("\n");
+		}
+
+		final String filename = "service-logs-" + dateString;
+		final ByteArrayInputStream contentStream = new ByteArrayInputStream(sb.toString().getBytes());
+
+		return new StreamResponse() {
+			@Override
+			public String getContentType() {
+				return "text/plain";
+			}
+
+			@Override
+			public InputStream getStream() throws IOException {
+				return contentStream;
+			}
+
+			@Override
+			public void prepareResponse(Response response) {
+				response.setHeader("Content-Disposition", "attachment; filename=" + filename);
+			}
+		};
+
+	}
 
 }
